7fd2c4fff49c563d302174af28820c5eff4f467c
Baback Khademi <bob.khademi@gmail.com>
2022-11-26 23:45:09 +0330

    Middle of Session 2

--- cypress/e2e/integration/session2.cy.js
+++ cypress/e2e/integration/session2.cy.js
@@ -1,7 +1,17 @@
 /// <reference types="cypress"/>
 
 describe('Session 2 Test Suite - Selecting DOM elements', () => {
-    it('should visit play2 website', () => {
-        cy.visit('http://play2.automationcamp.ir')
-    })
+    it('should visit play2 website and type babackkk', () => {
+        cy.visit('http://play2.automationcamp.ir');
+        cy.get('#fname').type('Babackkk');
+    }),
+
+        /*  it('should visit tables website and scroll horizontally', () => {
+             cy.visit('https://datatables.net/examples/basic_init/scroll_x.html');
+             cy.get('#example').scrollIntoView().scrollTo('right')
+         }); */
+        it('should test contains selector', () => {
+            cy.visit('https://datatables.net/examples/basic_init/scroll_x.html');
+            cy.get('#example').scrollIntoView().scrollTo('right')
+        });
 });
